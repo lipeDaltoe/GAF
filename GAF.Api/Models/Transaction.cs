12e3322e1be9efd61f1e36439477c8b2bed3fbbd
lipeDaltoe <felipedaltoe9@gmail.com>
2025-08-15 10:47:54 -0300

    por hoje é só... sextou!!🎉

---
+++ GAF.Api/Models/Transaction.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GAF.Api.Models;
+
+[Table("transactions")]
+public class Transaction
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string Description { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public TransactionType Type { get; set; }
+    
+    [Required]
+    public DateTime TransactionDate { get; set; }
+
+    [StringLength(1000)]
+    public string Notes { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    
+    [Required]
+    public string UserId { get; set; }
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
